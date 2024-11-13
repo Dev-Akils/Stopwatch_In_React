d407137851ed515642d71d8548c07246b4ace11d
Git-Akila <118921510+Git-Akila@users.noreply.github.com>
2024-11-13 21:15:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,26 +8,27 @@ import { useToken } from './Components/Login/useToken';
 import ChartData from './Api/ChartData';
  import DashboardKyc from '../src/Pages/DashboardKyc';
 
-
+import  StopWatch from './Components/Components1'
 
 
 function App() {
-  const { token, setToken } = useToken();
+  // const { token, setToken } = useToken();
  
   return (
-    <Router>
-      {/* <Header /> */}
-      <Routes>
-        {/* Redirect to Dashboard if token exists, otherwise show Login */}
-        <Route path="/login" element={!token ? <Login setToken={setToken} /> : <Navigate to="/" />} />
-        {/* Redirect to Login if no token, otherwise show Dashboard */}
-        <Route path="/" element={token ? <Dashboard /> : <Navigate to="/login" />} />
-        <Route path="/kyc" element={<DashboardKyc/>}/>
+    // <Router>
+     
+    //   <Routes>
+      
+    //     <Route path="/login" element={!token ? <Login setToken={setToken} /> : <Navigate to="/" />} />
+       
+    //     <Route path="/" element={token ? <Dashboard /> : <Navigate to="/login" />} />
+    //     <Route path="/kyc" element={<DashboardKyc/>}/>
         
         
 
-      </Routes>
-    </Router>
+    //   </Routes>
+    // </Router>
+   < StopWatch/>
   );
 }
 
