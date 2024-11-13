09dd2834d609a4fa6e250bbb9c454f5a3fcb96e7
Maharajan-hub <maharaja182409@gmail.com>
2024-11-13 21:35:15 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import './App.css';
 
-// import Dashboard from './Pages/Dashboard';
-// import Header from './Components/Header/Header'; 
-// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-// import Login from './Components/Login/login'; 
-// import { useToken } from './Components/Login/useToken';
-// import ChartData from './Api/ChartData';
-//  import DashboardKyc from '../src/Pages/DashboardKyc';
+
 
 import  StopWatch from './Components1'
 
 
 function App() {
-  // const { token, setToken } = useToken();
+ 
  
   return (
-    // <Router>
-     
-    //   <Routes>
-      
-    //     <Route path="/login" element={!token ? <Login setToken={setToken} /> : <Navigate to="/" />} />
-       
-    //     <Route path="/" element={token ? <Dashboard /> : <Navigate to="/login" />} />
-    //     <Route path="/kyc" element={<DashboardKyc/>}/>
-        
-        
-
-    //   </Routes>
-    // </Router>
+   
    < StopWatch/>
   );
 }
